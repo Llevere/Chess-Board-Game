ac96171a6bdf27710bca3cedf89ec7fd60a0d465
M-Levere <mattlevere1@gmail.com>
2024-03-06 18:40:43 -0500

    Rook highlighting tiles is working as it should so far

--- highlightSquares/highlightRookSquares.js
+++ highlightSquares/highlightRookSquares.js
@@ -30,11 +30,88 @@ function highlightRookSquares(position) {
   }
 
   //Check all columns within the same row to the left of the rook
-  for (let i = fromTileLocation.row; i > 0; i--) {}
+  for (let i = fromTileLocation.column - 1; i > 0; i--) {
+    //const square = document.getElementById(`${i}${fromTileLocation.row}`);
+    if (
+      !(
+        document.getElementById(`${i}${fromTileLocation.row}`).children.length >
+        0
+      )
+    ) {
+      document
+        .getElementById(`${i}${fromTileLocation.row}`)
+        .classList.add("hint");
+    }
+    if (
+      document.getElementById(`${i}${fromTileLocation.row}`).children.length > 0
+    ) {
+      if (
+        document.getElementById(`${i}${fromTileLocation.row}`)
+          .classList[3][0] !== fromColour
+      ) {
+        document
+          .getElementById(`${i}${fromTileLocation.row}`)
+          .classList.add("attack");
+      }
+      break;
+    }
+  }
+
+  if (fromTileLocation.row + 1 < 9) {
+    //Check all columns within the same row to the right of the rook
+    for (let i = fromTileLocation.row + 1; i < 9; i++) {
+      if (
+        !(
+          document.getElementById(`${fromTileLocation.column}${i}`).children
+            .length > 0
+        )
+      ) {
+        document
+          .getElementById(`${fromTileLocation.column}${i}`)
+          .classList.add("hint");
+      }
+      if (
+        document.getElementById(`${fromTileLocation.column}${i}`).children
+          .length > 0
+      ) {
+        if (
+          document.getElementById(`${fromTileLocation.column}${i}`)
+            .classList[3][0] !== fromColour
+        ) {
+          document
+            .getElementById(`${fromTileLocation.column}${i}`)
+            .classList.add("attack");
+        }
+        break;
+      }
+    }
+  }
 
   //Check all columns within the same row to the right of the rook
-  for (let i = fromTileLocation.column; i < 9; i++) {}
-
-  //Check all columns within the same row to the right of the rook
-  for (let i = fromTileLocation.column; i > 0; i--) {}
+  for (let i = fromTileLocation.row - 1; i > 0; i--) {
+    if (
+      !(
+        document.getElementById(`${fromTileLocation.column}${i}`).children
+          .length > 0
+      )
+    ) {
+      document
+        .getElementById(`${fromTileLocation.column}${i}`)
+        .classList.add("hint");
+    }
+    if (
+      document.getElementById(`${fromTileLocation.column}${i}`).children
+        .length > 0
+    ) {
+      if (
+        document.getElementById(`${fromTileLocation.column}${i}`)
+          .classList[3][0] !== fromColour
+      ) {
+        document
+          .getElementById(`${fromTileLocation.column}${i}`)
+          .classList.add("attack");
+      }
+      break;
+    }
+  }
 }
